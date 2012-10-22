93dfd3f0fada06132a78769377234b9882262ca9
Nicholas Escalona <nicholas.escalona@gmail.com>
2012-10-22 16:28:51 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mhatan::Application.config.secret_token = 'fa3516abf6c914b9703e8d352261c5ffb385bd2be6f72851784b2888316230891b6d0439befcd58a908de65c2de0448a0aa275949f82c9393704c8e590829233'
